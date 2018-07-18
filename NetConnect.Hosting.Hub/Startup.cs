cffdd8626a31c2f1af933bc9a8e95e45bf75694a
Burak Başar <basarburak@outlook.com>
2018-07-18 10:32:00 +0300

    Identity Server 4

--- NetConnect.Hosting.Hub/Startup.cs
+++ NetConnect.Hosting.Hub/Startup.cs
@@ -23,7 +23,6 @@ namespace NetConnect.Hosting.Hub
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
---
+++ NetConnect.Hosting.Public/Controllers/IdentityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace NetConnect.Hosting.Public.Controllers
+{
+    [Route("[controller]")]
+    [Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+        }
+    }
+}
